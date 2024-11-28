83769ef3f3724a0df0508811c1482dc9b8dec54a
marz <marzvrover@ieee.org>
2024-11-28 07:00:44 +0000

    introduce evil bug

--- buggy_pyheap/min_heap.py
+++ buggy_pyheap/min_heap.py
@@ -3,7 +3,7 @@ from .heap import Heap
 class MinHeap(Heap):
     def heapify_up(self):
         index = self.size - 1
-        while self.has_parent(index) and self.storage[index] < self.get_parent(index):
+        while self.has_parent(index) and self.storage[index] > self.get_parent(index):
             parent_index = self.get_parent_index(index)
             self.swap(parent_index, index)
             index = parent_index
@@ -12,7 +12,7 @@ class MinHeap(Heap):
         index = 0
         while self.has_left_child(index):
             smaller_child_index = self.get_left_child_index(index)
-            if self.has_right_child(index) and self.get_right_child(index) < self.get_left_child(index):
+            if self.has_right_child(index) and self.get_right_child(index) > self.get_left_child(index):
                 smaller_child_index = self.get_right_child_index(index)
             if self.storage[index] < self.storage[smaller_child_index]:
                 break
