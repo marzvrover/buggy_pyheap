b67b2071688e601e3f214eef2c6341b67be1bf63
marz <marz@marzvrover.com>
2024-11-28 00:27:48 -0500

    init

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='buggy-pyheap',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    tests_require=[
+        'pytest',
+    ],
+    author='Marz Rover',
+    author_email='marzvrover@ieee.org',
+    description='A puropsefully buggy heap implementation. Perfect for learning how to debug!',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/marzvrover/buggy-pyheap',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
